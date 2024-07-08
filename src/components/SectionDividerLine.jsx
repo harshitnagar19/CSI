0bbdd1d051c2227e07a1dada536c6af538ae0d62
Pranjal birla <birlapranjal460@gmail.com>
2024-07-08 23:04:10 +0530

    changes bg color of divider

--- src/components/SectionDividerLine.jsx
+++ src/components/SectionDividerLine.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function SectionDividerLine() {
   return (
     <div
-      className={`my-14 mx-auto bg-blue-lightLine w-[70%] h-1 rounded-full max-w-[100rem]`}
+      className={`my-14 mx-auto bg-gray-300 w-[70%] h-1 rounded-full max-w-[100rem]`}
     ></div>
   );
 }
