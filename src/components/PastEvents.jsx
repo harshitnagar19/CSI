4eceafd63f85a7ace76606beea8a5c1517cc8faa
harshit nagar <nharshit7024@gmail.com>
2024-07-20 14:38:02 +0530

    fix cards of past events

--- src/components/PastEvents.jsx
+++ src/components/PastEvents.jsx
@@ -6,25 +6,20 @@ import Marquee from "react-fast-marquee";
 import "./pastEvent.css";
 // cards
 const Card = ({ image, name, position, domain }) => (
-  <div className="text-black rounded-3xl border-4 flex flex-col items-center w-full sm:w-[250px] lg:w-[300px] aspect-[3/4] overflow-hidden">
-    <img
-      src={image}
-      alt={name}
-      className="w-full h-[70%] object-cover"
-    />
-    <div className="p-4 text-center w-full bg-white">
-      <h3 className="text-xl lg:text-2xl font-semibold truncate">{name}</h3>
-      {position && <p className="text-sm lg:text-base truncate">{position}</p>}
-      {domain && <p className="text-sm lg:text-base truncate">{domain}</p>}
-    </div>
+  <div class="card w-[200px] sm:w-[250px] lg:w-[300px] aspect-[3/4] ">
+    <img src={image} alt={name} className="w-full h-[70%] rounded-md object-cover" />
+    <p class="heading">{name}</p>
+    {position && <p >{position}</p>}
+    {domain && <p >{domain}</p>}
   </div>
+  
 );
 
 export default function PastEvents() {
   return (
     <div className=" text-white min-h-screen py-8  mx-auto">
       <h1 className="text-3xl lg:text-5xl font-mono mb-8">Past Events</h1>
-      
+
       <div className="mb-12">
         <h2 className="text-2xl lg:text-4xl font-mono mb-4">Kaushal</h2>
         <p className="text-sm lg:text-xl font-mono">
@@ -37,50 +32,50 @@ export default function PastEvents() {
 
       <div className="mb-12">
         <h2 className="text-2xl lg:text-4xl font-mono mb-4">Winners</h2>
-       
+
         <Marquee
-         gradient={true}
-         gradientColor={"rgba(0,0,0,0.9)"}
-         gradientWidth={170}
-         className="rounded-3xl"
-         speed={80}
-        //  pauseOnHover={true}
-         pauseOnClick={true}
-         delay={0}
-         play={true}
-         direction="left"
+          gradient={true}
+          gradientColor={"rgba(0,0,0,0.9)"}
+          gradientWidth={170}
+          className="rounded-3xl"
+          speed={80}
+          //  pauseOnHover={true}
+          pauseOnClick={true}
+          delay={0}
+          play={true}
+          direction="left"
         >
-        <div className="flex gap-16 justify-around ">
-          {winners.map((winner, index) => (
-            <Card key={index} {...winner} />
-          ))}
-        </div>
+          <div className="flex gap-16 justify-around ">
+            {winners.map((winner, index) => (
+              <Card key={index} {...winner} />
+            ))}
+          </div>
         </Marquee>
-        
+
       </div>
-      <SectionDividerLine/>
+      <SectionDividerLine />
 
       {/* Other Events */}
 
       <div className="mb-8">
         <h2 className="text-2xl lg:text-4xl font-mono mb-4">Other Events</h2>
         <Marquee
-         gradient={true}
-         gradientColor={"rgba(0,0,0,0.9)"}
-         gradientWidth={170}
-         className="rounded-3xl"
-         speed={80}
-        //  pauseOnHover={true}
-         pauseOnClick={true}
-         delay={0}
-         play={true}
-         direction="right"
+          gradient={true}
+          gradientColor={"rgba(0,0,0,0.9)"}
+          gradientWidth={170}
+          className="rounded-3xl"
+          speed={80}
+          //  pauseOnHover={true}
+          pauseOnClick={true}
+          delay={0}
+          play={true}
+          direction="right"
         >
-        <div className="flex flex-wrap justify-center gap-6 mb-6">
-          {otherEvents.map((event, index) => (
-            <Card key={index} {...event} />
-          ))}
-        </div>
+          <div className="flex flex-wrap justify-center gap-16 mb-6">
+            {otherEvents.map((event, index) => (
+              <Card key={index} {...event} />
+            ))}
+          </div>
         </Marquee>
         <div className="flex justify-center">
           <button className="bg-[#194EEF] text-white py-2 px-6 rounded-full text-lg hover:bg-blue-700 transition duration-300">
@@ -89,9 +84,9 @@ export default function PastEvents() {
         </div>
 
 
-        <SectionDividerLine/>
+        <SectionDividerLine />
       </div>
-      
+
     </div>
   );
 }
