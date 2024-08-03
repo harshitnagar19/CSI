5faf1607d4d1932c02c17fdfe797cab8db1e4d80
sohelxshaikh <sohailhacker7786@gmail.com>
2024-08-03 22:56:02 +0530

    modified:   src/components/heroSection/Hero.jsx 	modified:   src/ui/hero-parallax.jsx

--- src/components/heroSection/Hero.jsx
+++ src/components/heroSection/Hero.jsx
@@ -9,105 +9,107 @@ function Hero() {
   const handleScroll = (e) => {
     const { scrollTop, scrollHeight, clientHeight } = e.target;
     const position = Math.ceil(
-        (scrollTop / (scrollHeight - clientHeight)) * 100
+      (scrollTop / (scrollHeight - clientHeight)) * 100
     );
     setScrollPosition(position);
     console.log(scrollPosition)
-};
+  };
 
   return (
-<div 
-onScroll={handleScroll}
-className={'hero-class'}
->
-    <Navbar/>
-    <HeroParallax products={products} 
-    
-    />
+    <div
+      onScroll={handleScroll}
+      className={'hero-class'}
+    >
+      <Navbar />
+      <HeroParallax products={products}
+
+      />
     </div>
   )
 }
 export const products = [
-    {
-      title: "Moonbeam",
-      
-      thumbnail:
-        "https://aceternity.com/images/products/thumbnails/new/moonbeam.png",
-    },
-    {
-      title: "Cursor",
-      
-      thumbnail:
-        "https://aceternity.com/images/products/thumbnails/new/cursor.png",
-    },
-    {
-      title: "Rogue",
-      thumbnail:
-        "https://aceternity.com/images/products/thumbnails/new/rogue.png",
-    },
-   
-    {
-      title: "Editorially",
-      thumbnail:
-        "https://aceternity.com/images/products/thumbnails/new/editorially.png",
-    },
-    {
-      title: "Editrix AI",
-      thumbnail:
-        "https://aceternity.com/images/products/thumbnails/new/editrix.png",
-    },
-    {
-      title: "Pixel Perfect",
-      thumbnail:
-        "https://aceternity.com/images/products/thumbnails/new/pixelperfect.png",
-    },
-   
-    {
-      title: "Algochurn",
-      thumbnail:
-        "https://aceternity.com/images/products/thumbnails/new/algochurn.png",
-    },
-    {
-      title: "Aceternity UI",
-      thumbnail:
-        "https://aceternity.com/images/products/thumbnails/new/aceternityui.png",
-    },
-    {
-      title: "Tailwind Master Kit",
-      thumbnail:
-        "https://aceternity.com/images/products/thumbnails/new/tailwindmasterkit.png",
-    },
-    {
-      title: "SmartBridge",
-      thumbnail:
-        "https://aceternity.com/images/products/thumbnails/new/smartbridge.png",
-    },
-    {
-      title: "Renderwork Studio",
-      thumbnail:
-        "https://aceternity.com/images/products/thumbnails/new/renderwork.png",
-    },
-   
-    {
-      title: "Creme Digital",
-      thumbnail:
-        "https://aceternity.com/images/products/thumbnails/new/cremedigital.png",
-    },
-    {
-      title: "Golden Bells Academy",
-      thumbnail:
-        "https://aceternity.com/images/products/thumbnails/new/goldenbellsacademy.png",
-    },
-    {
-      title: "Invoker Labs",
-      thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/invoker.png",
-    },
-    {
-      title: "E Free Invoice",
-      thumbnail:
-        "https://aceternity.com/images/products/thumbnails/new/efreeinvoice.png",
-    },
-  ];
+  {
+    title: "Moonbeam",
+
+    thumbnail:
+      "https://scontent.frpr5-1.fna.fbcdn.net/v/t39.30808-6/394407854_661821139386077_3393138086445287603_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=f727a1&_nc_ohc=iSgaFrDwcHEQ7kNvgEnH9rG&_nc_ht=scontent.frpr5-1.fna&oh=00_AYBVhO0Et92K8O11B54T9I5JJAb2gBrMUlI76KmGHA09Gw&oe=66B44E74",
+  },
+  {
+    title: "Editorially",
+    thumbnail:
+      "https://scontent.frpr5-1.fna.fbcdn.net/v/t39.30808-6/394524100_661820062719518_5553354846524753359_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=f727a1&_nc_ohc=02NErEg4SI8Q7kNvgHTxPg8&_nc_ht=scontent.frpr5-1.fna&oh=00_AYDi6lJ3gi09NSh8yNnrOkdmxX2hsGtRc_TFz1lNXyBaeA&oe=66B42D7D",
+  },
+  {
+    title: "Cursor",
+
+    thumbnail:
+      "https://scontent.frpr5-1.fna.fbcdn.net/v/t39.30808-6/394355842_661784109389780_6328665765551797346_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=f727a1&_nc_ohc=xFJ9l5vMg2wQ7kNvgFC3FPx&_nc_ht=scontent.frpr5-1.fna&oh=00_AYBfhZzHDnt-nRlnK2O68dTVKihC3mwp3YNd7pZbA6sxiQ&oe=66B41E4F",
+  },
+  {
+    title: "Rogue",
+    thumbnail:
+      "https://scontent.frpr5-1.fna.fbcdn.net/v/t39.30808-6/394710436_661788846055973_1708568801802530305_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=f727a1&_nc_ohc=J7IfKJf-2YQQ7kNvgHZZBHy&_nc_ht=scontent.frpr5-1.fna&oh=00_AYADi5_x7pJwjlIFfrx6SBpqAWJNCHL-56dUogr2OhkITQ&oe=66B43642",
+  },
+  {
+    title: "Editrix AI",
+    thumbnail:
+      "https://scontent.frpr5-1.fna.fbcdn.net/v/t39.30808-6/394036454_661841236050734_2755256402745406275_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=f727a1&_nc_ohc=DjksH8TQaSkQ7kNvgGoAB3b&_nc_ht=scontent.frpr5-1.fna&oh=00_AYAXLAT4C81QWJMvqwez5aPyc-A1vNyAdROLKMSp1up6BA&oe=66B43FBD",
+  },
+
+
+
+  {
+    title: "Pixel Perfect",
+    thumbnail:
+      "https://scontent.frpr5-1.fna.fbcdn.net/v/t39.30808-6/394392430_661786349389556_8448857218706356288_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=f727a1&_nc_ohc=VXwqfDyyorgQ7kNvgGzaHJF&_nc_ht=scontent.frpr5-1.fna&oh=00_AYC48lnqgJZ0GPXP2ANblC-IKxlrKhMTmPJuwad-_y_8Qg&oe=66B43EF9",
+  },
+
+  {
+    title: "Algochurn",
+    thumbnail:
+      "https://scontent.frpr5-1.fna.fbcdn.net/v/t39.30808-6/394039579_661844839383707_5675017185449611946_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=f727a1&_nc_ohc=ODHhlLr04_AQ7kNvgFdmmnC&_nc_ht=scontent.frpr5-1.fna&oh=00_AYC8ycP2vsGiShL8bwPDtdLeFz9OnPfAfWZBEfFiJGf-kw&oe=66B42FC3",
+  },
+  {
+    title: "Aceternity UI",
+    thumbnail:
+      "https://scontent.frpr5-1.fna.fbcdn.net/v/t39.30808-6/394681895_661820252719499_6412697551067781787_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=f727a1&_nc_ohc=roztkExHDbMQ7kNvgHkltWr&_nc_ht=scontent.frpr5-1.fna&oh=00_AYCl-HZJwyCtfQ06U0FdFvOiAPqo-uHc0iTLmCTROo7xqg&oe=66B451D8",
+  },
+  {
+    title: "Tailwind Master Kit",
+    thumbnail:
+      "https://scontent.frpr5-1.fna.fbcdn.net/v/t39.30808-6/394452189_661845079383683_8105138942443232701_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=f727a1&_nc_ohc=rGoe_p8zwt8Q7kNvgEBykYR&_nc_ht=scontent.frpr5-1.fna&oh=00_AYCzwgr8jtG9BEeFOn5Ccf8lBc1JZGXw5oCt_mGgOzMasw&oe=66B42A5E",
+  },
+  {
+    title: "SmartBridge",
+    thumbnail:
+      "https://scontent.frpr5-1.fna.fbcdn.net/v/t39.30808-6/394330098_662377655997092_6739174882297173122_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=f727a1&_nc_ohc=mWDOztzQRawQ7kNvgEhsrRC&_nc_ht=scontent.frpr5-1.fna&oh=00_AYCKobsi8v7qvB9gw2Px0FJhVD4YsLfvkw9R4VdBpDza1w&oe=66B45031",
+  },
+  {
+    title: "Renderwork Studio",
+    thumbnail:
+      "https://scontent.frpr5-1.fna.fbcdn.net/v/t39.30808-6/394051584_661845576050300_6865418083907683418_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=f727a1&_nc_ohc=ZmaxFRTk8OcQ7kNvgFXbxBO&_nc_ht=scontent.frpr5-1.fna&oh=00_AYCzWnoG3GbwlgazY1RgwlXm6CjwXspNuIXHL4KOsTP53A&oe=66B42813",
+  },
+
+  {
+    title: "Creme Digital",
+    thumbnail:
+      "https://scontent.frpr5-1.fna.fbcdn.net/v/t39.30808-6/394380905_662377562663768_4057887480950409180_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=f727a1&_nc_ohc=505BGY-MMyIQ7kNvgGVTh03&_nc_ht=scontent.frpr5-1.fna&oh=00_AYDGtY5AeVA2RmXqzYxNzxAiWENkLiQticUBa0YnUbzfSg&oe=66B44144",
+  },
+  {
+    title: "Golden Bells Academy",
+    thumbnail:
+      "https://scontent.frpr5-1.fna.fbcdn.net/v/t39.30808-6/394324020_662376655997192_7829603275687121864_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=f727a1&_nc_ohc=PkqmouIU2qgQ7kNvgFdLixJ&_nc_ht=scontent.frpr5-1.fna&oh=00_AYB12rPeCmhZeDnhTQJNUbpVLSrAAlgjh8-DyZnGguUTRg&oe=66B446C9",
+  },
+  {
+    title: "Invoker Labs",
+    thumbnail:
+      "https://scontent.frpr5-1.fna.fbcdn.net/v/t39.30808-6/394005428_661844526050405_3430944101914743479_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=f727a1&_nc_ohc=idk6JUZ7GE4Q7kNvgFwnJJx&_nc_ht=scontent.frpr5-1.fna&oh=00_AYCQkPAfFfOtHntfqrOJujAJ6HIjqN29uWwIg0_QCCQVow&oe=66B439C6",
+  },
+  {
+    title: "E Free Invoice",
+    thumbnail:
+      "https://scontent.frpr5-1.fna.fbcdn.net/v/t39.30808-6/394072293_661842149383976_8551315070898668561_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=f727a1&_nc_ohc=Jcjp86bKVagQ7kNvgGoBUzU&_nc_ht=scontent.frpr5-1.fna&oh=00_AYCuLtwvaIdr5lnh5ec_Q66xApjItDD1sWO6AFTFr-GOsg&oe=66B43F14",
+  },
+];
 
 export default Hero;
--- src/ui/hero-parallax.jsx
+++ src/ui/hero-parallax.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState ,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import {
   motion,
   useScroll,
@@ -16,7 +16,7 @@ export const HeroParallax = ({
   const firstRow = products.slice(0, 5);
   const secondRow = products.slice(5, 10);
   const thirdRow = products.slice(10, 15);
-  const [bgClass , setBgClass] = useState("bg-black")
+  const [bgClass, setBgClass] = useState("bg-black")
   const ref = React.useRef(null);
   const { scrollYProgress } = useScroll({
     target: ref,
@@ -63,7 +63,7 @@ export const HeroParallax = ({
   return (
     <div
       ref={ref}
-      className={`${bgClass} h-[300vh]  overflow-hidden  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]`}
+      className={`${bgClass} h-[380vh]  overflow-hidden  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]`}
     >
       <Header />
       <motion.div
@@ -93,7 +93,7 @@ export const HeroParallax = ({
             />
           ))}
         </motion.div>
-        <motion.div className="flex flex-row-reverse space-x-reverse space-x-20">
+        <motion.div className="flex flex-row-reverse space-x-reverse space-x-10">
           {thirdRow.map((product) => (
             <ProductCard
               product={product}
@@ -110,20 +110,22 @@ export const HeroParallax = ({
 export const Header = () => {
   return (
     <>
-    <div className=" max-w-7xl relative  mx-auto py-10 md:py-20 px-4 w-full  left-0 top-0">
-      <h1 className="text-2xl md:text-7xl font-bold text-white mt-20">
-        The Ultimate <br /> development studio
-      </h1>
-      <p className="max-w-2xl text-base md:text-xl mt-8 text-neutral-200">
-        We build beautiful products with the latest technologies and frameworks.
-        We are a team of passionate developers and designers that love to build
-        amazing products.
-      </p>
-      <div className="relative z-20 flex justify-center mt-20">
-        <button className="join-us-btn">Join Us <i class="fa fa-arrow-right" aria-hidden="true"></i>
-      </button></div>
-    </div>
-    
+      <div className=" max-w-7xl relative  mx-auto py-10 md:py-20 px-4 w-full  left-0 top-0">
+        <h1 className="text-2xl md:text-7xl font-bold text-white mt-20">
+          Computer Socity of India - SVVV <br /> Tech Community
+        </h1>
+        <p className="max-w-6xl text-base md:text-xl mt-8 text-neutral-200">
+
+
+          The Computer Society of India (CSI) is the oldest and largest organization of IT and allied professionals in India. As a non-profit professional society, CSI is dedicated to computing, facilitating the exchange of views and information, and fostering learning and idea-sharing.
+
+          CSI Shri Vaishnav Vidyapeeth Vishwavidyalaya (SVVV) connects you with the broader CSI community, providing you with opportunities to engage with recent technological advancements and innovations. Through CSI-SVVV, you can stay informed about the latest developments in IT and enhance your skills in the ever-evolving field of technology.
+        </p>
+        <div className="relative z-20 flex justify-center mt-20">
+          <a href="https://chat.whatsapp.com/DGfZeut8mD1B8Kz47aRgch"> <button className="join-us-btn">Join Us <i class="fa fa-arrow-right" aria-hidden="true"></i>
+          </button></a></div>
+      </div>
+
     </>
   );
 };
@@ -155,10 +157,10 @@ export const ProductCard = ({
           alt={product.title}
         />
       </a>
-      <div className="absolute inset-0 h-full w-full opacity-0 group-hover/product:opacity-80 bg-black pointer-events-none"></div>
+      {/* <div className="absolute inset-0 h-full w-full opacity-0 group-hover/product:opacity-80 bg-pink-600 pointer-events-none"></div>
       <h2 className="absolute bottom-4 left-4 opacity-0 group-hover/product:opacity-100 text-white">
         {product.title}
-      </h2>
+      </h2> */}
     </motion.div>
   );
 };
