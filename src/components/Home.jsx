f53b8de2629346e9ca25d6be242470a731a086d3
harshit nagar <nharshit7024@gmail.com>
2024-08-16 14:43:27 +0530

    addding confiti

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,14 +6,35 @@ import UpcomingEvents from "./UpcomingEventa";
 import Team from "./Team";
 import { TracingBeam } from "../ui/tracing-beam";
 import Footer from "./footer/Footer";
+import { useWindowSize, } from '@react-hook/window-size'
+import Confetti from 'react-confetti'
 function Home() {
-
-
+    const [width, height] = useWindowSize()
     return (
         <div  >
+
             <div
                 className="w-full dark:bg-white bg-black  dark:bg-grid-black/[0.2] bg-grid-white/[0.2]"
             >
+                <Confetti
+                    width={width - 100}
+                    height={height}
+                    recycle={false}
+                    initialVelocityY={17}
+                    gravity={0.5}
+                    numberOfPieces={600}
+                    drawShape={ctx => {
+                        ctx.beginPath()
+                        for (let i = 0; i < 22; i++) {
+                            const angle = 0.35 * i
+                            const x = (0.2 + (1.5 * angle)) * Math.cos(angle)
+                            const y = (0.2 + (1.5 * angle)) * Math.sin(angle)
+                            ctx.lineTo(x, y)
+                        }
+                        ctx.stroke()
+                        ctx.closePath()
+                    }}
+                />
                 <Hero />
                 <AboutCollege />
                 <TracingBeam>
@@ -21,7 +42,7 @@ function Home() {
                 </TracingBeam>
                 <UpcomingEvents />
                 <Team />
-                <Footer/>
+                <Footer />
             </div>
         </div>
     )
